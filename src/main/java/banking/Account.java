c2e7bb167fe8db9b396d7a3a34d453b33400852e
Bryan Melo <bmelo650@gmail.com>
2023-08-16 13:21:48 -0700

    Main menu completed

--- src/main/java/banking/Account.java
+++ src/main/java/banking/Account.java
@@ -1,41 +1,43 @@
 package src.main.java.banking;
 
-public class Account
-{
+public class Account {
     private final String accountHolderName;
     private double balance;
     private final int accountNumber;
 
     // Constructor
-    protected Account(String name, double initialDeposit, int accountNumber)
-    {
+    protected Account(String name, double initialDeposit, int accountNumber) {
         this.accountHolderName = name;
         this.balance = initialDeposit;
         this.accountNumber = accountNumber;
     }
 
     // Getters
-    protected int getAccountNumber() { return this.accountNumber; }
-    public String getAccountHolderName() { return this.accountHolderName; }
-    public double getBalance() { return this.balance; }
+    protected int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return this.accountHolderName;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
 
     // Deposit money into account
-    public void deposit(double amount)
-    {
+    public void deposit(double amount) {
         this.balance += amount;
         System.out.printf("%s has successfully deposited $%.2f\n", accountHolderName, amount);
     }
 
     // Withdraw money from account
-    public void withdraw(double amount)
-    {
-        if ((this.balance - amount) >= 0)
-        {
+    public void withdraw(double amount) {
+        if ((this.balance - amount) >= 0) {
             this.balance -= amount;
             System.out.printf("%s has successfully withdrawn $%.2f\n", accountHolderName, amount);
-        }
-        else
-            System.out.printf("Cannot withdraw $%.2f because your balance is $%.2f\n",  amount, balance);
+        } else
+            System.out.printf("Cannot withdraw $%.2f because your balance is $%.2f\n", amount, balance);
     }
 
 }
--- src/main/java/banking/Bank.java
+++ src/main/java/banking/Bank.java
@@ -1,25 +1,20 @@
 package src.main.java.banking;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
-public class Bank
-{
+public class Bank {
     private final ArrayList<Account> listOfAccounts;
     private int accountNumber;
 
     // Constructor
-    public Bank()
-    {
+    public Bank() {
         this.listOfAccounts = new ArrayList<Account>();
         this.accountNumber = 0;
     }
 
     // Getters - Get Account based on account number
-    public Account getAccount(int accountNum)
-    {
-        for (Account account : this.listOfAccounts)
-        {
+    public Account getAccount(int accountNum) {
+        for (Account account : this.listOfAccounts) {
             if (account.getAccountNumber() == accountNum)
                 return account;
         }
@@ -29,21 +24,17 @@ public class Bank
 
     // Create account using name and initial deposit
     // Account number is assigned and returned
-    public int createAccount(String name, double initialDeposit)
-    {
+    public int createAccount(String name, double initialDeposit) {
         this.listOfAccounts.add(new Account(name, initialDeposit, ++this.accountNumber));
         return this.accountNumber;
     }
 
     // Removes account based on account number
-    public void deleteAccount(int accountNum)
-    {
-        if (getAccount(accountNum) != null)
-        {
-            this.listOfAccounts.remove(getAccount(accountNum));
+    public void deleteAccount(Account accountToBeDeleted) {
+        if (listOfAccounts.contains(accountToBeDeleted)) {
+            this.listOfAccounts.remove(accountToBeDeleted);
             System.out.println("Account has been removed");
-        }
-        else
+        } else
             System.out.println("Account does not exist");
     }
 }
